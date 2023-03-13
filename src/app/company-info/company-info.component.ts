946384b6f4803329c61a6cc71e63556250d84b9f
nodariVadatchkoria <n.vadatchkoria@alte.edu.ge>
2023-03-14 03:46:25 +0400

    text editro

--- src/app/company-info/company-info.component.ts
+++ src/app/company-info/company-info.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import {faAlignLeft, faAlignCenter, faAlignRight, faTextSlash, faUnderline, faItalic, faBold,  } from '@fortawesome/free-solid-svg-icons';
+import {Component, OnInit} from '@angular/core';
+import {
+  faAlignLeft,
+  faAlignCenter,
+  faAlignRight,
+  faTextSlash,
+  faUnderline,
+  faItalic,
+  faBold,
+} from '@fortawesome/free-solid-svg-icons';
+import {NgStyle} from "@angular/common";
+import {Observable, of, Subject} from "rxjs";
+import {style} from "@angular/animations";
 
 @Component({
   selector: 'app-company-info',
   templateUrl: './company-info.component.html',
-  styleUrls: ['./company-info.component.scss']
+  styleUrls: ['./company-info.component.scss'],
 })
 export class CompanyInfoComponent implements OnInit {
   faAlignLeft = faAlignLeft;
@@ -14,86 +25,104 @@ export class CompanyInfoComponent implements OnInit {
   faUnderline = faUnderline;
   faItalic = faItalic;
   faBold = faBold;
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
   }
-  textarea:any = document.getElementById("textarea1");
-  e:any = document.getElementById("f8");
-  f1(e:any) {
-    const value = e.value;
-    this.textarea.style.fontSize = value + "px";
-  }
-  f2(e:any) {
-    if (this.textarea.style.fontWeight == "bold") {
-      this.textarea.style.fontWeight = "normal";
-      e.classList.remove("active");
-    }
-    else {
-      this.textarea.style.fontWeight = "bold";
-      e.classList.add("active");
-    }
-  }
- f3(e:any) {
-    if (this.textarea.style.fontStyle == "italic") {
-      this.textarea.style.fontStyle = "normal";
-      e.classList.remove("active");
-    }
-    else {
-      this.textarea.style.fontStyle = "italic";
-      e.classList.add("active");
-    }
-  }
-  f4(e:any) {
-    if (this.textarea.style.textDecoration == "underline") {
-      this.textarea.style.textDecoration = "none";
-      e.classList.remove("active");
-    }
-    else {
-      this.textarea.style.textDecoration = "underline";
-      e.classList.add("active");
-    }
-  }
-  f5(e:any) {
-    this.textarea.style.textAlign = "left";
-  }
-  f6(e:any) {
-    this.textarea.style.textAlign = "center";
-  }
-  f7(e:any) {
-    this.textarea.style.textAlign = "right";
-  }
-  f8(e:any) {
-    if (this.textarea.style.textTransform == "uppercase") {
-      this.textarea.style.textTransform = "none";
-      this.e.classList.remove("active");
-    }
-    else {
-      this.textarea.style.textTransform = "uppercase";
-      this.e.classList.add("active");
-    }
-  }
-  f9() {
-    this.textarea.style.fontWeight = "normal";
-    this.textarea.style.textAlign = "left";
-    this.textarea.style.fontStyle = "normal";
-    this.textarea.style.textTransform = "capitalize";
-    this.textarea.value = "";
-  }
-//   f10(e:any) {
-//     let value:any | undefined = e.value;
-//     this.textarea.style.color = value;
-//   }
+
+
+
+  //  f8(e:any) {
+  //    if (this.textarea.style.textTransform == "uppercase") {
+  //      this.textarea.style.textTransform = "none";
+  //      e.classList.remove("active");
+  //    }
+  //    else {
+  //      this.textarea.style.textTransform = "uppercase";
+  //      e.classList.add("active");
+  //    }
+  //  }
+  //  f9() {
+  //    this.textarea.style.fontWeight = "normal";
+  //    this.textarea.style.textAlign = "left";
+  //    this.textarea.style.fontStyle = "normal";
+  //    this.textarea.style.textTransform = "capitalize";
+  //    this.textarea.value = "";
+  //  }
+  //  f10(e:any) {
+  //     let value= e.value;
+  //    this.textarea.style.color = value;
+  //  }
 //   window.addEventListener('load', (
 //
 // ) => {
 //  this.textarea = "";
 // });
-
-
-
+//
+//
+//
 //   window.addEventListener('load', () => {
 //   this.textarea.value = "";
 // });
+  bold: boolean = false;
+  italic: boolean = false;
+  underline: boolean = false;
+  alignleft: boolean = false;
+  aligncenter: boolean = false;
+  alignright: boolean = false;
 
+  fontsize11 :any = 16;
+
+
+  changeFontWeight() {
+    this.bold = !this.bold;
+  }
+
+  changeFontWeight2() {
+    this.italic = !this.italic;
+
+  }
+
+  changeFontWeight3() {
+    this.underline = !this.underline;
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+
+  fontSizeChange() {
+    let fontSize = document.getElementById("fontSize") as HTMLInputElement;
+    let fontSizeValue = fontSize.value;
+    this.fontsize11 = fontSizeValue + "px";
+localStorage.setItem("fontsize", this.fontsize11);
+
+  }
+
+
+  alignLeft() {
+
+    this.alignleft = !this.alignleft;
+  }
+
+  alignCenter() {
+    this.aligncenter = !this.aligncenter;
+  }
+
+  alignRight() {
+    this.alignright = !this.alignright;
+  }
+
+  aA() {
+
+  }
+
+  capitalize() {
+
+  }
+
+  color() {
+
+  }
 }
